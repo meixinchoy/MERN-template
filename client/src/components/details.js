ff08e454fd521de1d9c8d2ecda1aa43de74b647a
meixin <meixinchoy@gmail.com>
2022-03-05 10:13:22 +0800

    changed to hooks

--- client/src/components/details.js
+++ client/src/components/details.js
@@ -1,68 +1,58 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const baseURL = "http://localhost:3000/";
 
-export default class Details extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: [],
-            buttonClicked: false
-        };
-        this.getDetails = this.getDetails.bind(this);
-    }
+const Details = () => {
 
-    componentDidMount() {
+    const [data, setData] = useState([]);
+    const [buttonClicked, setButtonClicked] = useState([]);
+
+    useEffect(() => {
         axios.get(baseURL + "getData").then(res => {
-            this.setState({
-                data: res.data
-            });
-            console.log(res.data)
+            setData(res.data);
+            console.log(res.data);
         });
-    }
+    })
 
-    getDetails() {
-        this.setState({
-            buttonClicked: !this.state.buttonClicked
-        });
+    const getDetails = () => {
+        setButtonClicked(!buttonClicked)
     }
-
-    render() {
-        return (
-            <div className="container-fluid" style={{ marginTop: "30px" }}>
-                <div className="row">
-                    <div className="col-xs-12" style={{ textAlign: " center" }}>
-                        <button
-                            className="btn btn-primary"
-                            onClick={this.getDetails}
-                        >
-                            Click
-                        </button>
-                        <div
-                            className="container-fluid"
-                            style={{
-                                textAlign: "center",
-                            }}
-                        >
-                            {this.state.buttonClicked ? this.state.data.map(data => {
-                                return (
-                                    <React.Fragment>
-                                        <p>
-                                            <b>name</b> : {data.name}
-                                        </p>
-                                        <p>
-                                            <b>age</b> : {data.age}
-                                        </p>
-                                        <hr />
-                                    </React.Fragment>
-                                );
-                            })
-                                : null}
-                        </div>
+    return (
+        <div className="container-fluid" style={{ marginTop: "30px" }}>
+            <div className="row">
+                <div className="col-xs-12" style={{ textAlign: " center" }}>
+                    <button
+                        className="btn btn-primary"
+                        onClick={getDetails}
+                    >
+                        Click
+                    </button>
+                    <div
+                        className="container-fluid"
+                        style={{
+                            textAlign: "center",
+                        }}
+                    >
+                        {buttonClicked ? data.map(d => {
+                            return (
+                                <React.Fragment>
+                                    <p>
+                                        <b>name</b> : {d.name}
+                                    </p>
+                                    <p>
+                                        <b>age</b> : {d.age}
+                                    </p>
+                                    <hr />
+                                </React.Fragment>
+                            );
+                        })
+                            : null}
                     </div>
                 </div>
-            </div >
-        );
-    }
+            </div>
+        </div >
+    );
 }
+
+export default Details;
